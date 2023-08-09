85552508842431de0d60dcee36bba7e5af12c40e
Chesney Hardin <chesney.hardin@gmail.com>
2023-08-09 15:54:55 -0500

    added delete buttons for events and games

--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -62,6 +62,12 @@ class GameView(ViewSet):
         game.save()
 
         return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        game = Game.objects.get(pk=pk)
+        game.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+        
 
 
 class GameCreatorSerializer(serializers.ModelSerializer):
